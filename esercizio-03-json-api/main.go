8267f4e22d46c47e114cdfa88c7f8e8b0992d4e4
Marco Widesott <marco.widesott@anitia.it>
2026-02-04 12:03:20 +0100

    init ex 3

--- esercizio-03-json-api/main.go
+++ esercizio-03-json-api/main.go
@@ -1,14 +1,68 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"sync"
+	"time"
 )
 
-func main() {
-	// TODO: Implementare il JSON API Server
+type Book struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Author      string    `json:"author"`
+	ISBN        string    `json:"isbn"`
+	PublishYear int       `json:"publish_year"`
+	CreatedAt   time.Time `json:"created_at"`
+}
 
-	fmt.Println("Starting JSON API Server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+type BookStore struct {
+	mu     sync.RWMutex
+	books  map[string]Book
+	nextID int64
+}
+
+func main() {
+	store := &BookStore{
+		books: make(map[string]Book),
+	}
+
+}
+
+func (s *BookStore) Get(id string) (Book, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	b, ok := s.books[id]
+
+	return b, ok
+}
+
+func (s *BookStore) List() []Book {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	list := make([]Book, 0, len(s.books))
+	for _, b := range s.books {
+		list = append(list, b)
+	}
+
+	return list
+}
+
+func (s *BookStore) Create(b Book) Book {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+}
+
+func (s *BookStore) Update(id string, b Book) (Book, bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+}
+
+func (s *BookStore) Delete(id string) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, ok := s.books[id]; !ok {
+		return false
+	}
+	delete(s.books, id)
+
+	return true
 }
