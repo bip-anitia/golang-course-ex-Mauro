b78ee22af1f9d94dc9a7b277a72ec9a83946f8cc
Marco Widesott <marco.widesott@anitia.it>
2026-02-04 14:32:28 +0100

    init ex 4

--- esercizio-04-cli-flags/main.go
+++ esercizio-04-cli-flags/main.go
@@ -3,7 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+
+	"github.com/spf13/cobra"
 )
+
+var rootCmd = &cobra.Command{
+	Use:   "filetools",
+	Short: "A versatile file processing tool",
+}
+
+var countCmd = &cobra.Command{
+	Use:   "count [files...]",
+	Short: "Count lines, words, and characters",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// TODO: implement
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(countCmd)
+}
 
 func main() {
 	// TODO: Definire e parsare i flags
