d4c4e75d2151808b57db4172669a706c9e018257
Marco Widesott <marco.widesott@anitia.it>
2026-02-04 15:57:20 +0100

    init ex 5

--- esercizio-05-rate-limiter/main.go
+++ esercizio-05-rate-limiter/main.go
@@ -2,9 +2,60 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+type TokenBucketLimiter struct {
+	tokens     chan struct{}
+	ticker     *time.Ticker
+	maxTokens  int
+	refillRate time.Duration
+}
 
 func main() {
 	// TODO: Implementare il rate limiter
 	fmt.Println("Rate Limiter")
+}
+
+func NewTokenBucketLimiter(maxTokens int, refillRate time.Duration) *TokenBucketLimiter {
+	tokens := make(chan struct{}, maxTokens)
+	for i := 0; i < maxTokens; i++ {
+		tokens <- struct{}{}
+	}
+
+	ticker := time.NewTicker(refillRate)
+	rl := &TokenBucketLimiter{
+		tokens:     tokens,
+		ticker:     ticker,
+		maxTokens:  maxTokens,
+		refillRate: refillRate,
+	}
+
+	go func() {
+		for range ticker.C {
+			select {
+			case rl.tokens <- struct{}{}:
+			default:
+			}
+		}
+	}()
+
+	return rl
+}
+
+func (rl *TokenBucketLimiter) Wait() {
+	<-rl.tokens
+}
+
+func (rl *TokenBucketLimiter) TryWait(timeout time.Duration) bool {
+	select {
+	case <-rl.tokens:
+		return true
+	case <-time.After(timeout):
+		return false
+	}
+}
+
+func (rl *TokenBucketLimiter) Stop() {
+	rl.ticker.Stop()
 }
