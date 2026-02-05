916adfffca95333984f9ef2cc1c088f2ad569b41
Marco Widesott <marco.widesott@anitia.it>
2026-02-05 16:53:33 +0100

    ended ex 6

--- esercizio-06-worker-pool/main.go
+++ esercizio-06-worker-pool/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 type Task struct {
@@ -54,7 +57,58 @@ func (wp *WorkerPool) worker(id int) {
 	}
 }
 
+func (wp *WorkerPool) Submit(task Task) {
+	wp.tasks <- task
+}
+
+func (wp *WorkerPool) Results() <-chan Result {
+	return wp.results
+}
+
+func (wp *WorkerPool) Stop() {
+	close(wp.tasks)
+	wp.wg.Wait()
+	close(wp.results)
+}
+
 func main() {
-	// TODO: Implementare il worker pool
-	fmt.Println("Worker Pool Pattern")
+	workers := flag.Int("workers", 5, "number of workers")
+	tasks := flag.Int("tasks", 100, "number of tasks")
+	flag.Parse()
+	start := time.Now()
+	var total, success, failed int64
+
+	pool := NewWorkerPool(*workers)
+	pool.Start()
+	defer pool.Stop()
+
+	numTasks := *tasks
+	go func() {
+		for i := 0; i < numTasks; i++ {
+			task := Task{ID: i, Data: i, Process: func(d interface{}) (interface{}, error) {
+				time.Sleep(100 * time.Millisecond)
+				return fmt.Sprintf("done %v", d), nil
+			}}
+			pool.Submit(task)
+		}
+	}()
+
+	for i := 0; i < numTasks; i++ {
+		res := <-pool.Results()
+		fmt.Printf("Task %d: %v (err=%v)\n", res.TaskID, res.Value, res.Error)
+		atomic.AddInt64(&total, 1)
+		if res.Error != nil {
+			atomic.AddInt64(&failed, 1)
+		} else {
+			atomic.AddInt64(&success, 1)
+		}
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("\nStatistics:\n")
+	fmt.Printf("  Total tasks: %d\n", total)
+	fmt.Printf("  Successful: %d\n", success)
+	fmt.Printf("  Failed: %d\n", failed)
+	fmt.Printf("  Total time: %s\n", elapsed)
+	fmt.Printf("  Throughput: %.2f tasks/sec\n", float64(total)/elapsed.Seconds())
+
 }
