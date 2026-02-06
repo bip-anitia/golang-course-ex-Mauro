c36866ac16e6980c652d083bd27453a5d49ec7d5
Marco Widesott <marco.widesott@anitia.it>
2026-02-06 12:04:48 +0100

    implemented withTimeoutExample ex 8

--- esercizio-08-context/main.go
+++ esercizio-08-context/main.go
@@ -1,6 +1,11 @@
 package main
 
-func withTimeoutExample()           { /* context.WithTimeout + select */ }
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
 func withCancellationExample()      { /* context.WithCancel + ticker */ }
 func workerPoolWithContextExample() { /* workers che ascoltano ctx.Done() */ }
 func pipelineWithContextExample()   { /* stage che esce su ctx.Done() */ }
@@ -14,4 +19,21 @@ func main() {
 	withValueExample()
 }
 
+func withTimeoutExample() {
+	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancel()
 
+	resultCh := make(chan string, 1)
+
+	go func() {
+		time.Sleep(1 * time.Second) // lavoro lento
+		resultCh <- "completed"
+	}()
+
+	select {
+	case result := <-resultCh:
+		fmt.Println("timeout example:", result)
+	case <-ctx.Done():
+		fmt.Println("timeout example:", ctx.Err())
+	}
+}
